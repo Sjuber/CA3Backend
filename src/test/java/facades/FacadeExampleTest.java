72d6ab93fc1eb8166297a7402a18ef4cf2e1aca1
Sjuber <54933648+Sjuber@users.noreply.github.com>
2021-04-23 15:49:37 +0200

    all the rest

---
+++ src/test/java/facades/FacadeExampleTest.java
@@ -0,0 +1,91 @@
+package facades;
+
+import entities.Role;
+import entities.User;
+import io.restassured.http.ContentType;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import utils.EMF_Creator;
+import entities.RenameMe;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//Uncomment the line below, to temporarily disable this test
+//@Disabled
+public class FacadeExampleTest {
+
+    private static EntityManagerFactory emf;
+    private static FacadeExample facade;
+
+    public FacadeExampleTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+       emf = EMF_Creator.createEntityManagerFactoryForTest();
+       facade = FacadeExample.getFacadeExample(emf);
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
+    }
+
+    // Setup the DataBase in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the code below to use YOUR OWN entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Role userRole = new Role("user");
+            Role adminRole = new Role("admin");
+            User user = new User("user", "test");
+            user.addRole(userRole);
+            User admin = new User("admin", "test");
+            admin.addRole(adminRole);
+            User both = new User("user_admin", "test");
+            both.addRole(userRole);
+            both.addRole(adminRole);
+           // em.persist(userRole);
+          //  em.persist(adminRole);
+            em.persist(user);
+            em.persist(admin);
+            em.persist(both);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+//        Remove any data after each test was run
+    }
+
+//    @Test
+//    public void testDummyMsg() throws Exception {
+//        given().contentType("application/json")
+//                .get("/5things/teachersSolution").then()
+//                .assertThat()
+//                .body(hasSize(3));
+//
+//    }
+
+    // TODO: Delete or change this method 
+//    @Test
+//    public void testAFacadeMethod() {
+//        assertEquals(2, facade.getRenameMeCount(), "Expects two rows in the database");
+//    }
+
+
+}
